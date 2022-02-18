6a0823a7e0bad99839e626ac29ebb2022256ca26
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-19 01:34:05 +0700

    Add files via upload

---
+++ shadowsocks/cekss.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${CYAN}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+echo "Checking VPS"
+clear
+echo "----------------------------------";
+echo "----=[ SS - OBFS User Login ]=----";
+echo "----------------------------------";
+echo ""
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+x=1
+echo "----------------------------------";
+echo " User | TLS"
+echo "----------------------------------";
+for akun in "${data[@]}"
+do
+port=$(cat /etc/shadowsocks-libev/akun.conf | grep '^port_tls' | cut -d ' ' -f 2 | tr '\n' ' ' | awk '{print $'"$x"'}')
+jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w $port | awk '{print $2}' | sort | uniq | nl)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+echo " $akun - $port"
+echo "$jum";
+echo "----------------------------------"
+fi
+x=$(( "$x" + 1 ))
+done
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+x=1
+echo ""
+echo "----------------------------------";
+echo " User |  No TLS"
+echo "----------------------------------";
+for akun in "${data[@]}"
+do
+port=$(cat /etc/shadowsocks-libev/akun.conf | grep '^port_http' | cut -d ' ' -f 2 | tr '\n' ' ' | awk '{print $'"$x"'}')
+jum=$(netstat -anp | grep ESTABLISHED | grep obfs-server | cut -d ':' -f 2 | grep -w $port | awk '{print $2}' | sort | uniq | nl)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+echo " $akun - $port"
+echo "$jum";
+echo "----------------------------------"
+fi
+x=$(( "$x" + 1 ))
+done
