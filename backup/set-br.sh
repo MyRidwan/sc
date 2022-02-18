c86642ea843f021a5dbc89ca78b09969d8b7e98b
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-19 01:17:46 +0700

    Add files via upload

---
+++ backup/set-br.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${CYAN}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+echo "Checking VPS"
+clear
+# Link Hosting Kalian
+akbarvpn="raw.githubusercontent.com/myridwan/sc/ipuk/backup"
+
+apt install rclone -y
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://${akbarvpn}/rclone.conf"
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user bckupvpns@gmail.com
+from bckupvpns@gmail.com
+password Yangbaru1 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://${akbarvpn}/autobackup.sh"
+wget -O backup "https://${akbarvpn}/backup.sh"
+wget -O restore "https://${akbarvpn}/restore.sh"
+wget -O strt "https://${akbarvpn}/strt.sh"
+wget -O limitspeed "https://${akbarvpn}/limitspeed.sh"
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+cd
+rm -f /root/set-br.sh
---
+++ backup/strt.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${CYAN}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+echo "Checking VPS"
+clear
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+printf "$user\n$exp2\n" | addss
+done
+rm -f /etc/shadowsocks-libev/ss.conf
+data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+chmod 777 /home/vps/public_html/$user.conf
+done
