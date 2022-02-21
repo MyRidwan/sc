90973a654c820bcacfd1194ddf862dd0ffbb31c3
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-21 18:11:35 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,12 +2,12 @@
 
 clear
 echo -e "================================================="
-echo -e "#     Premium Auto Script By RIDWAN STORE      #"
+echo -e "#     Premium Auto Script By RIDWAN STORE       #"
 echo -e "================================================="
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Build Up By RIDWANSTORE                     #"
+echo -e "# Build Up By RIDWANSTORE                       #"
 echo -e "================================================="
 echo -e "# Thanks To                                     #"
 echo -e "================================================="
