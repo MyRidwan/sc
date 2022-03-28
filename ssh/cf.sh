52fc941d11b952920440e8590b7c1db8bd9bd884
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-03-28 11:28:25 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-clear
-read -rp "Input Your Domain For This Server :" -e SUB_DOMAIN
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+apt install jq curl -y
+DOMAIN=nezavpn.my.id
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
+echo $sub > /root/cfku
+SUB_DOMAIN=${sub}.nezavpn.my.id
+CF_ID=neza.afdhan@gmail.com
+CF_KEY=144fa068a8533d3f4ce31fab53333bb04cac3
+echo "nezavpn.my.id" > /root/sdomain
+echo $SUB_DOMAIN > /root/subdomain
+
+set -euo pipefail
+IP=$(wget -qO- ipinfo.io/ip);
+echo "Record DNS ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
-# / / Make Main Directory
-mkdir -p /usr/bin/xray
-mkdir -p /etc/xray
-cp /root/domain /etc/xray
+
+#wget -O /usr/bin/dhn "https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/dhn.sh"
+#wget -O /usr/bin/dhn2 "https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/dhn2.sh"
+#wget -O /usr/bin/dhn3 "https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/dhn3.sh"
+#wget -O /usr/bin/nza "https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/nza.sh"
+#wget -O /usr/bin/wcc "https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/cs_wc.sh"
+
+#chmod +x /usr/bin/dhn
+#chmod +x /usr/bin/dhn2
+#chmod +x /usr/bin/dhn3
+#chmod +x /usr/bin/nza
+#chmod +x /usr/bin/wcc
+
+echo -e "Done Record Domain For VPS" | lolcat
+figlet -f slant ridwan | lolcat
+sleep 1
+
 rm -f /root/cf.sh
