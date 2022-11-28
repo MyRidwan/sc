bc09695be966fcc5d4fa79949e8f70ce2823b138
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-11-28 13:51:43 +0700

    Add files via upload

---
+++ backup/set-br.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# My Telegram : https://t.me/anuybazoelk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/Anuybazoelk639"
+echo -e "${NC}${LIGHT}WhatsApp : 085349326511"
+echo -e "${NC}${LIGHT}WhatsApp : 081774970898"
+echo -e "${NC}${LIGHT}Youtube : youtube.com/@nyarigratisan"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/anuybazoelk"
+echo -e "${NC}${LIGHT}Telegram : https;//t.me/r1f4n_112"
+exit 0
+fi
+# Link Hosting Kalian
+akbarvpn="raw.githubusercontent.com/myridwan/sc/main/backup"
+
+curl https://rclone.org/install.sh | bash
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://${akbarvpn}/rclone.conf"
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user anuybazoelk639@gmail.com
+from anuybazoelk639@gmail.com
+password 25Agustus@1990 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://${akbarvpn}/autobackup.sh"
+wget -O backup "https://${akbarvpn}/backup.sh"
+wget -O restore "https://${akbarvpn}/restore.sh"
+wget -O strt "https://${akbarvpn}/strt.sh"
+wget -O limitspeed "https://${akbarvpn}/limitspeed.sh"
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+cd
+rm -f /root/set-br.sh
---
+++ backup/strt.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# My Telegram : https://t.me/anuybazoelk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/Anuybazoelk639"
+echo -e "${NC}${LIGHT}WhatsApp : 085349326511"
+echo -e "${NC}${LIGHT}WhatsApp : 081774970898"
+echo -e "${NC}${LIGHT}Youtube : youtube.com/@nyarigratisan"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/anuybazoelk"
+echo -e "${NC}${LIGHT}Telegram : https;//t.me/r1f4n_112"
+exit 0
+fi
+clear
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+printf "$user\n$exp2\n" | addss
+done
+rm -f /etc/shadowsocks-libev/ss.conf
+data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+chmod 777 /home/vps/public_html/$user.conf
+done
