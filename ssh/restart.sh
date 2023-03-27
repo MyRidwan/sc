e56f0acd9e84a3cb5b0e64cbedc5d0da5b79064d
Myridwan <92353970+MyRidwan@users.noreply.github.com>
2023-03-28 05:11:16 +0700

    Update restart.sh

--- ssh/restart.sh
+++ ssh/restart.sh
@@ -44,3 +44,5 @@ screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
 echo -e "Restart All Service Berhasil"
+read -n 1 -s -r -p "   Press [ Enter ] to back on menu"
+menu
