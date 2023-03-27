05742049aedaa4a25f0ce3f593ec4d4be96a1ee1
Myridwan <92353970+MyRidwan@users.noreply.github.com>
2023-03-27 19:13:21 +0700

    Add files via upload

---
+++ ssh/sshovpn.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+clear
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+# COLOR VALIDATION
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+#warna
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m              ⇱ Menu SSH Ridwan ⇲               \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ 1) Create SSH & OpenVPN Account                                          "
+echo -e "❇️ 2) Create SSH & OpenVPN Trial                                             "
+echo -e "❇️ 3) SSH & OpenVPN Live                                                "
+echo -e "❇️ 4) Check User Login SSH                                                "
+echo -e "❇️ 5) Daftar Member SSH                                            "
+echo -e "❇️ 6) Delete SSH                                          "
+echo -e "❇️ 7) Delete User Expired SSH                                          "
+echo -e "❇️ 8) Set up Autokill SSH                                                 "
+echo -e "❇️ 9) Displays Users Multi Login                                               "
+echo -e "❇️ 10) Restart All Service                                         "
+echo -e "❇️ 11) Menu Utama                                            "
+echo -e "❇️ 12) EXIT                                                        "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m              ⇱ Script By Ridwan ⇲              \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+read -p "Select From Options [ 1 - 12 ] : " menu
+echo -e ""
+case $menu in
+1)
+addssh
+;;
+2)
+trialssh
+;;
+3)
+renewssh
+;;
+4)
+cekssh
+;;
+5)
+member
+;;
+6)
+delssh
+;;
+7)
+delexp
+;;
+8)
+autokill
+;;
+9)
+ceklim
+;;
+10)
+restart
+;;
+11)
+menu
+;;
+12)
+clear
+exit
+;;
+*)
+clear
+menu
+;;
+esac
